3b15405b0de428a7207d5b67019ae5b27978d95a
oririechman <ori@logz.io>
2016-11-09 21:14:51 -0600

    DEV-2858: use JMH

---
+++ sawmill-benchmark/src/main/java/io/logz/sawmill/benchmark/SawmillBenchmark.java
@@ -0,0 +1,12 @@
+package io.logz.sawmill.benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class SawmillBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        
+    }
+
+}
