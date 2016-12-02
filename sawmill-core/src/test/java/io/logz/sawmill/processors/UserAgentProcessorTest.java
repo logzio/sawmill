f7b71d42bf32fc277e9a44be32d0d96bd3c491e9
oririechman <ori@logz.io>
2016-12-01 19:03:42 -0600

    PR comments

--- sawmill-core/src/test/java/io/logz/sawmill/processors/UserAgentProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/UserAgentProcessorTest.java
@@ -18,7 +18,7 @@ public class UserAgentProcessorTest {
         Doc doc = createDoc(field, uaString);
 
         String config = "{ \"field\": \"" + field + "\" }";
-        UserAgentProcessor uaProceesor = (UserAgentProcessor) new UserAgentProcessor.Factory().create(config);
+        UserAgentProcessor uaProceesor = new UserAgentProcessor.Factory().create(config);
 
         ProcessResult processResult = uaProceesor.process(doc);
 
