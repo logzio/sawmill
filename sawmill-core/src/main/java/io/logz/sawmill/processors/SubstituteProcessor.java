49a1a761289b2fe183d0ef21436c3a250ffc0e76
oririechman <ori@logz.io>
2017-01-24 01:39:44 +0200

    DEV-3526: gsub processor

---
+++ sawmill-core/src/main/java/io/logz/sawmill/processors/SubstituteProcessor.java
@@ -0,0 +1,81 @@
+package io.logz.sawmill.processors;
+
+import io.logz.sawmill.Doc;
+import io.logz.sawmill.ProcessResult;
+import io.logz.sawmill.Processor;
+import io.logz.sawmill.annotations.ProcessorProvider;
+import io.logz.sawmill.exceptions.ProcessorParseException;
+import io.logz.sawmill.utilities.JsonUtils;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@ProcessorProvider(type = "gsub", factory = SubstituteProcessor.Factory.class)
+public class SubstituteProcessor implements Processor {
+
+    private final String field;
+    private final Pattern pattern;
+    private final String replacement;
+
+    public SubstituteProcessor(String field, Pattern pattern, String replacement) {
+        this.field = checkNotNull(field, "field cannot be null");
+        this.pattern = checkNotNull(pattern, "pattern cannot be null");
+        this.replacement = checkNotNull(replacement, "replacement cannot be null");
+    }
+
+    @Override
+    public ProcessResult process(Doc doc) {
+        if (!doc.hasField(field)) {
+            return ProcessResult.failure("failed to convert field in path [%s], field is missing");
+        }
+        String beforeSubstitute = doc.getField(field);
+        Matcher matcher = pattern.matcher(beforeSubstitute);
+        String afterSubstitute = matcher.replaceAll(replacement);
+
+        doc.addField(field, afterSubstitute);
+        return ProcessResult.success();
+    }
+
+    public static class Factory implements Processor.Factory {
+        public Factory() {
+        }
+
+        @Override
+        public SubstituteProcessor create(Map<String,Object> config) {
+            SubstituteProcessor.Configuration subConfig = JsonUtils.fromJsonMap(SubstituteProcessor.Configuration.class, config);
+
+            Pattern pattern;
+            try {
+                pattern = Pattern.compile(subConfig.getPattern());
+            } catch (PatternSyntaxException e) {
+                throw new ProcessorParseException("cannot create gsub processor with invalid pattern");
+            }
+
+            return new SubstituteProcessor(subConfig.getField(), pattern, subConfig.getReplacement());
+        }
+    }
+
+    public static class Configuration implements Processor.Configuration {
+        private String field;
+        private String pattern;
+        private String replacement;
+
+        public Configuration() { }
+
+        public String getField() {
+            return field;
+        }
+
+        public String getPattern() {
+            return pattern;
+        }
+
+        public String getReplacement() {
+            return replacement;
+        }
+    }
+}
---
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/SubstituteProcessorTest.java
@@ -0,0 +1,74 @@
+package io.logz.sawmill.processors;
+
+import io.logz.sawmill.Doc;
+import io.logz.sawmill.ProcessResult;
+import io.logz.sawmill.exceptions.ProcessorParseException;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.logz.sawmill.utils.DocUtils.createDoc;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SubstituteProcessorTest {
+
+    @Test
+    public void testSubstitute() {
+        String field = "message";
+        String message = "I'm g@nna \"remove\" $ome spec!al characters";
+
+        String pattern = "\\$|@|!|\\\"|'";
+        String replacement = ".";
+        Map<String, Object> config = new HashMap<>();
+        config.put("field", field);
+        config.put("pattern", pattern);
+        config.put("replacement", replacement);
+
+        Doc doc = createDoc(field, message);
+
+        SubstituteProcessor substituteProcessor = new SubstituteProcessor.Factory().create(config);
+
+        ProcessResult processResult = substituteProcessor.process(doc);
+
+        assertThat(processResult.isSucceeded()).isTrue();
+        assertThat((String) doc.getField(field)).isEqualTo("I.m g.nna .remove. .ome spec.al characters");
+    }
+
+    @Test
+    public void testFieldNotFound() {
+        String field = "message";
+        String message = "differnet field name";
+
+        String pattern = "\\$|@|!|\\\"|'";
+        String replacement = ".";
+        Map<String, Object> config = new HashMap<>();
+        config.put("field", field);
+        config.put("pattern", pattern);
+        config.put("replacement", replacement);
+
+        Doc doc = createDoc("differentFieldName", message);
+
+        SubstituteProcessor substituteProcessor = new SubstituteProcessor.Factory().create(config);
+
+        ProcessResult processResult = substituteProcessor.process(doc);
+
+        assertThat(processResult.isSucceeded()).isFalse();
+    }
+
+    @Test
+    public void testInvalidPattern() {
+        String field = "message";
+
+        String pattern = "\\";
+        Map<String, Object> config = new HashMap<>();
+        config.put("field", field);
+        config.put("pattern", pattern);
+        config.put("replacement", "");
+
+        Doc doc = createDoc(field, "value");
+
+        assertThatThrownBy(() -> new SubstituteProcessor.Factory().create(config)).isInstanceOf(ProcessorParseException.class);
+    }
+}
