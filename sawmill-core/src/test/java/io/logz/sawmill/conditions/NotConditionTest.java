398fb337a606f0bedce5a07d430e42bb05f5f829
Ziv Segal <ziv@logshero.com>
2017-03-19 16:21:11 +0200

    Fail with a clear error in case of bad ‘Not’ config

--- sawmill-core/src/test/java/io/logz/sawmill/conditions/NotConditionTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/conditions/NotConditionTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import java.util.Collections;
 
 import static io.logz.sawmill.utils.DocUtils.createDoc;
+import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NotConditionTest {
 
@@ -24,6 +26,14 @@ public class NotConditionTest {
         Doc doc = createDoc("field1", "value1");
 
         assertThat(notCondition.evaluate(doc)).isFalse();
+    }
+
+    @Test
+    public void testInvalidNotConfiguration() {
+        NotCondition.Factory factory = new NotCondition.Factory();
+        assertThatThrownBy(() -> factory.create(emptyMap(), null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("'Not' condition must contain a valid list of conditions, with at least one condition");
     }
 
 }
