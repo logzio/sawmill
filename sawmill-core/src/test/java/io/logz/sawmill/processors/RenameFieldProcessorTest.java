488f8d73943717e022cb011c28df15d129a7ecce
Ziv Segal <ziv@logshero.com>
2017-05-07 19:32:48 +0300

    Added test for rename of Json value

--- sawmill-core/src/test/java/io/logz/sawmill/processors/RenameFieldProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/RenameFieldProcessorTest.java
@@ -1,5 +1,6 @@
 package io.logz.sawmill.processors;
 
+import com.google.common.collect.ImmutableMap;
 import io.logz.sawmill.Doc;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
@@ -48,9 +49,9 @@ public class RenameFieldProcessorTest {
     public void testRenameWithTemplateInFrom() {
         Doc doc = createDoc("field-a", "field-b",
                             "field-b", "value-of-c");
-        String fromField = "{{field-a}}";
 
-        Map<String, Object> config = createConfig("from", fromField,
+        Map<String, Object> config = createConfig(
+                "from", "{{field-a}}",
                 "to", "field-c");
 
         RenameFieldProcessor renameFieldProcessor = createProcessor(RenameFieldProcessor.class, config);
@@ -65,8 +66,8 @@ public class RenameFieldProcessorTest {
         Doc doc = createDoc("field-a", "field-b",
                             "field-c", "value-of-c");
 
-        String fromField = "field-c";
-        Map<String, Object> config = createConfig("from", fromField,
+        Map<String, Object> config = createConfig(
+                "from", "field-c",
                 "to", "{{field-a}}");
 
         RenameFieldProcessor renameFieldProcessor = createProcessor(RenameFieldProcessor.class, config);
@@ -74,5 +75,25 @@ public class RenameFieldProcessorTest {
         assertThat(renameFieldProcessor.process(doc).isSucceeded()).isTrue();
 
         assertThat((String)doc.getField("field-b")).isEqualTo("value-of-c");
+    }
+
+    @Test
+    public void testRenameJsonWithTemplateInTo() {
+        Map valueBeingRename = ImmutableMap.of("x", 5);
+
+        Doc doc = createDoc(
+                "field-a", "field-b",
+                "field-c", valueBeingRename);
+
+        Map<String, Object> config = createConfig(
+                "from", "field-c",
+                "to", "{{field-a}}");
+
+        RenameFieldProcessor renameFieldProcessor = createProcessor(RenameFieldProcessor.class, config);
+
+        assertThat(renameFieldProcessor.process(doc).isSucceeded()).isTrue();
+
+        Map renamedValue = doc.getField("field-b");
+        assertThat(renamedValue.get("x")).isEqualTo(5);
     }
 }
