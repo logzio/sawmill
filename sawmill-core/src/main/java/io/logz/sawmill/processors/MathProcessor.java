4b8d6d204d899373c40fc7b4567d9debdc422c3f
oririechman <ori@logz.io>
2017-05-24 22:58:16 +0300

    DEV-3622: math processor

--- sawmill-core/src/main/java/io/logz/sawmill/processors/MathProcessor.java
+++ sawmill-core/src/main/java/io/logz/sawmill/processors/MathProcessor.java
@@ -1,5 +1,6 @@
 package io.logz.sawmill.processors;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.primitives.Doubles;
 import io.logz.sawmill.Doc;
 import io.logz.sawmill.ProcessResult;
@@ -7,14 +8,11 @@ import io.logz.sawmill.Processor;
 import io.logz.sawmill.Template;
 import io.logz.sawmill.TemplateService;
 import io.logz.sawmill.annotations.ProcessorProvider;
-import io.logz.sawmill.exceptions.ProcessorExecutionException;
 import io.logz.sawmill.utilities.JsonUtils;
 
 import javax.inject.Inject;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.util.Map;
+import java.util.function.BiFunction;
 
 import static java.util.Objects.requireNonNull;
 
@@ -22,30 +20,19 @@ import static java.util.Objects.requireNonNull;
 public class MathProcessor implements Processor {
     private final String targetField;
     private final Template expressionTemplate;
-    private final ScriptEngine engine;
+    private ExpressionEvaluator expressionEvaluator;
 
     public MathProcessor(String targetField, Template expressionTemplate) {
         this.targetField = requireNonNull(targetField, "target field cannot be null");
         this.expressionTemplate = requireNonNull(expressionTemplate, "expression cannot be null");
-        ScriptEngineManager engineManager = new ScriptEngineManager();
-        engine = engineManager.getEngineByName("JavaScript");
+        expressionEvaluator = new ExpressionEvaluator();
     }
 
     @Override
     public ProcessResult process(Doc doc) {
-        Double result;
         String expression = expressionTemplate.render(doc);
 
-        if (!expression.matches("[0-9+\\-*\\/\\(\\)\\^. ]+")) {
-            return ProcessResult.failure("invalid expression");
-        }
-
-        try {
-            result = Doubles.tryParse(engine.eval(expression).toString());
-        } catch (ScriptException e) {
-            return ProcessResult.failure("failed to run expression",
-                    new ProcessorExecutionException("math", e));
-        }
+        Double result = expressionEvaluator.eval(expression);
 
         if (result == null) {
             return ProcessResult.failure("invalid expression");
@@ -84,6 +71,84 @@ public class MathProcessor implements Processor {
 
         public String getExpression() {
             return expression;
+        }
+    }
+
+    public static class ExpressionEvaluator {
+        private final Map<Character, BiFunction<Double,Double,Double>> operationsMap = new ImmutableMap.Builder<Character, BiFunction<Double,Double,Double>>()
+                .put('+', (a,b) -> a+b)
+                .put('-', (a,b) -> a-b)
+                .put('/', (a,b) -> a/b)
+                .put('*', (a,b) -> a*b)
+                .build();
+
+        public Double eval(String expression) {
+            String exp = trimParentheses(expression.trim());
+            Double value = Doubles.tryParse(exp);
+            if (value != null) return value;
+            int operatorIndex = findOperationIndex(exp);
+            if (operatorIndex == -1) {
+                return null;
+            }
+            String leftExp = exp.substring(0, operatorIndex);
+            String rightExp = exp.substring(operatorIndex+1, exp.length());
+            char operation = exp.charAt(operatorIndex);
+            if (!operationsMap.containsKey(operation)) {
+                return null;
+            }
+
+            Double left = eval(leftExp);
+            Double right = eval(rightExp);
+            if (left == null || right == null) {
+                return null;
+            }
+            return operationsMap.get(operation).apply(left, right);
+        }
+        private int findOperationIndex(String exp) {
+            int i;
+            for (i=0;i<exp.length();i++) {
+                char charAt = exp.charAt(i);
+                if (operationsMap.keySet().contains(charAt)) return i;
+                if (charAt == '(') {
+                    int parenthesesCount = 1;
+                    i++;
+                    while (parenthesesCount > 0 && i < exp.length()) {
+                        char skippedCharAt = exp.charAt(i);
+                        if (skippedCharAt == '(') {
+                            parenthesesCount++;
+                        } else if (skippedCharAt == ')') {
+                            parenthesesCount--;
+                        }
+                        i++;
+                    }
+                }
+            }
+            if (i >= exp.length()) return -1;
+            return i;
+        }
+
+        private String trimParentheses(String input) {
+            if (input.charAt(0) == '(') {
+                int closingParenthesisIndex = getClosingParenthesisIndex(input);
+                if (closingParenthesisIndex == input.length()-1)
+                    return trimParentheses(input.substring(1, input.length()-1));
+            }
+            return input;
+        }
+        private int getClosingParenthesisIndex(String input) {
+            int parenthesesCount = 1;
+            int i = 1;
+            while (parenthesesCount > 0 && i < input.length()) {
+                char skippedCharAt = input.charAt(i);
+                if (skippedCharAt == '(') {
+                    parenthesesCount++;
+                } else if (skippedCharAt == ')') {
+                    parenthesesCount--;
+                }
+                if (parenthesesCount == 0) return i;
+                i++;
+            }
+            return -1; //invalid input string
         }
     }
 }
--- sawmill-core/src/test/java/io/logz/sawmill/processors/MathProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/MathProcessorTest.java
@@ -13,7 +13,7 @@ public class MathProcessorTest {
     @Test
     public void testExpressionWithFields() {
         String targetField = "sum";
-        String expression = "{{field1}} + {{field2}} * 10 / ((5 + {{field3}})^2)";
+        String expression = "{{field1}} + {{field2}} * 10 / (5 + {{field3}})";
         Doc doc = createDoc("field1", 5,
                 "field2", 10.5d,
                 "field3", 7l);
@@ -23,7 +23,7 @@ public class MathProcessorTest {
         ProcessResult processResult = mathProcessor.process(doc);
 
         assertThat(processResult.isSucceeded()).isTrue();
-        assertThat((Double) doc.getField(targetField)).isEqualTo(5 + 10.5d * 10 / ((5 + 7)^2));
+        assertThat((Double) doc.getField(targetField)).isEqualTo(5 + 10.5d * 10 / (5 + 7));
     }
 
     @Test
@@ -58,15 +58,35 @@ public class MathProcessorTest {
     }
 
     @Test
-    public void testJavaScriptExpression() {
+    public void testInvalidExpressionWithFields() {
         String targetField = "sum";
-        String expression = "var i = [1,2,3]; i.forEach(function(v) { console.log(v + 1); });";
-        Doc doc = createDoc("field1", 5);
+        String expression = "{{field1}} + {{field2}} * 10 / (5 + {{field3}})";
+        Doc doc = createDoc("field1", "five",
+                "field2", 10.5d,
+                "field3", 7l);
 
         MathProcessor mathProcessor = createProcessor(MathProcessor.class, "expression", expression, "targetField", targetField);
 
         ProcessResult processResult = mathProcessor.process(doc);
 
         assertThat(processResult.isSucceeded()).isFalse();
+    }
+
+    @Test
+    public void testExpressionEvaluator() {
+        MathProcessor.ExpressionEvaluator evaluator = new MathProcessor.ExpressionEvaluator();
+        assertThat(evaluator.eval("5")).isEqualTo(5);
+        assertThat(evaluator.eval("5.7")).isEqualTo(5.7);
+        assertThat(evaluator.eval("(5.5)")).isEqualTo(5.5);
+        assertThat(evaluator.eval("(5+1)")).isEqualTo(6);
+        assertThat(evaluator.eval("(5.7-1.2)")).isEqualTo(4.5);
+        assertThat(evaluator.eval(" ( 5.7 - 1.2 ) ")).isEqualTo(4.5);
+        assertThat(evaluator.eval("(5.7*1.2)")).isEqualTo(5.7*1.2);
+        assertThat(evaluator.eval("(6.6/6)")).isEqualTo(6.6/6);
+        assertThat(evaluator.eval("(6.6/(2+4))")).isEqualTo(6.6/6);
+        assertThat(evaluator.eval("(6.6/ ( 2 + (2*2) ))")).isEqualTo(6.6/6);
+        assertThat(evaluator.eval("(5.7*1.2*3.4)")).isEqualTo(5.7*1.2*3.4);
+        assertThat(evaluator.eval("(5.7 - 1.2) - 3.4")).isEqualTo((5.7) - (1.2) - (3.4));
+        assertThat(evaluator.eval("((5.7-1.2) - 3.4)")).isEqualTo((5.7) - (1.2) - (3.4));
     }
 }
