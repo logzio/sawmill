e5623201cd3aefbc0201f5c6986f4873aa18818e
oririechman <ori@logz.io>
2017-05-25 23:00:47 +0300

    DEV-3622: math processor

--- sawmill-core/src/main/java/io/logz/sawmill/processors/MathProcessor.java
+++ sawmill-core/src/main/java/io/logz/sawmill/processors/MathProcessor.java
@@ -1,59 +1,110 @@
 package io.logz.sawmill.processors;
 
+import com.google.common.primitives.Doubles;
 import io.logz.sawmill.Doc;
 import io.logz.sawmill.ProcessResult;
 import io.logz.sawmill.Processor;
-import io.logz.sawmill.Template;
-import io.logz.sawmill.TemplateService;
 import io.logz.sawmill.annotations.ProcessorProvider;
-import io.logz.sawmill.utilities.ExpressionEvaluator;
+import io.logz.sawmill.exceptions.ProcessorConfigurationException;
 import io.logz.sawmill.utilities.JsonUtils;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import net.objecthunter.exp4j.ValidationResult;
 
-import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.Objects.requireNonNull;
 
 @ProcessorProvider(type = "math", factory = MathProcessor.Factory.class)
 public class MathProcessor implements Processor {
     private final String targetField;
-    private final Template expressionTemplate;
-    private ExpressionEvaluator expressionEvaluator;
+    private final Expression expression;
+    private final Set<String> variables;
 
-    public MathProcessor(String targetField, Template expressionTemplate) {
+    public MathProcessor(String targetField, Expression expression, Set<String> variables) {
         this.targetField = requireNonNull(targetField, "target field cannot be null");
-        this.expressionTemplate = requireNonNull(expressionTemplate, "expression cannot be null");
-        expressionEvaluator = new ExpressionEvaluator();
+        this.expression = requireNonNull(expression, "expression cannot be null");
+        this.variables = variables;
     }
 
     @Override
     public ProcessResult process(Doc doc) {
-        String expression = expressionTemplate.render(doc);
+        Map<String, Double> variablesMap = new HashMap<>();
 
-        Double result = expressionEvaluator.calculate(expression);
+        for (String variable : variables) {
+            if (!doc.hasField(variable)) {
+                return ProcessResult.failure("field [%s] is missing");
+            }
 
-        if (result == null) {
-            return ProcessResult.failure("invalid expression");
+            Double value = null;
+            Object fieldValue = doc.getField(variable);
+            if (fieldValue instanceof Number) {
+                value = ((Number) fieldValue).doubleValue();
+            } else if (fieldValue instanceof String) {
+                value = Doubles.tryParse((String) fieldValue);
+            }
+
+            if (value == null) {
+                return ProcessResult.failure("field [%s] is not a number");
+            }
+
+            variablesMap.put(variable, value);
         }
 
-        doc.addField(targetField, result);
+        expression.setVariables(variablesMap);
+
+        try {
+            doc.addField(targetField,  expression.evaluate());
+        } catch (ArithmeticException e) {
+            return ProcessResult.failure("Division by zero!");
+        }
 
         return ProcessResult.success();
     }
 
     public static class Factory implements Processor.Factory {
-        private final TemplateService templateService;
-
-        @Inject
-        public Factory(TemplateService templateService) {
-            this.templateService = templateService;
-        }
+        private final Pattern mustachePattern = Pattern.compile("\\{\\{(.+?)\\}\\}");
 
         @Override
         public MathProcessor create(Map<String,Object> config) {
             MathProcessor.Configuration mathConfig = JsonUtils.fromJsonMap(MathProcessor.Configuration.class, config);
 
-            return new MathProcessor(mathConfig.getTargetField(), templateService.createTemplate(mathConfig.getExpression()));
+            Set<String> variables = findVariables(mathConfig.getExpression());
+
+            Expression expression;
+            try {
+                 expression = new ExpressionBuilder(trimMustache(mathConfig.getExpression()))
+                        .variables(variables)
+                        .build();
+            } catch (IllegalArgumentException e) {
+                throw new ProcessorConfigurationException("invalid expression");
+            }
+
+            if (!expression.validate(false).isValid()) {
+                throw new ProcessorConfigurationException("invalid expression");
+            }
+
+            return new MathProcessor(mathConfig.getTargetField(), expression, variables);
+        }
+
+        private String trimMustache(String expression) {
+            return expression.replaceAll("(\\{\\{|\\}\\})", "");
+        }
+
+        private Set<String> findVariables(String expression) {
+            Set<String> variables = new HashSet<>();
+            Matcher matcher = mustachePattern.matcher(expression);
+
+            while (matcher.find()) {
+                variables.add(matcher.group(1));
+            }
+
+            return variables;
         }
     }
 
--- sawmill-core/src/test/java/io/logz/sawmill/processors/MathProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/MathProcessorTest.java
@@ -2,11 +2,13 @@ package io.logz.sawmill.processors;
 
 import io.logz.sawmill.Doc;
 import io.logz.sawmill.ProcessResult;
+import io.logz.sawmill.exceptions.ProcessorConfigurationException;
 import org.junit.Test;
 
 import static io.logz.sawmill.utils.DocUtils.createDoc;
 import static io.logz.sawmill.utils.FactoryUtils.createProcessor;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MathProcessorTest {
 
@@ -44,17 +46,12 @@ public class MathProcessorTest {
 
     @Test
     public void testInvalidExpression() {
-        String targetField = "sum";
-        String expression = "5 + 10.5 * 10 / (5 + 7";
-        Doc doc = createDoc("field1", 5,
-                "field2", 10.5d,
-                "field3", 7l);
-
-        MathProcessor mathProcessor = createProcessor(MathProcessor.class, "expression", expression, "targetField", targetField);
-
-        ProcessResult processResult = mathProcessor.process(doc);
-
-        assertThat(processResult.isSucceeded()).isFalse();
+        assertThatThrownBy(() -> createProcessor(MathProcessor.class,
+                "expression", "5 + 10.5 * 10 / (5 + 7",
+                "targetField", "sum")).isInstanceOf(ProcessorConfigurationException.class);
+        assertThatThrownBy(() -> createProcessor(MathProcessor.class,
+                "expression", "5 / five * (5 + 7)",
+                "targetField", "sum")).isInstanceOf(ProcessorConfigurationException.class);
     }
 
     @Test
@@ -68,6 +65,24 @@ public class MathProcessorTest {
         MathProcessor mathProcessor = createProcessor(MathProcessor.class, "expression", expression, "targetField", targetField);
 
         ProcessResult processResult = mathProcessor.process(doc);
+
+        assertThat(processResult.isSucceeded()).isFalse();
+    }
+
+    @Test
+    public void testDividingByZero() {
+        String targetField = "sum";
+        Doc doc = createDoc("field1", 0);
+
+        String expression = "5 / 0";
+        MathProcessor mathProcessor = createProcessor(MathProcessor.class, "expression", expression, "targetField", targetField);
+        ProcessResult processResult = mathProcessor.process(doc);
+
+        assertThat(processResult.isSucceeded()).isFalse();
+
+        expression = "5 / {{field1}}";
+        mathProcessor = createProcessor(MathProcessor.class, "expression", expression, "targetField", targetField);
+        processResult = mathProcessor.process(doc);
 
         assertThat(processResult.isSucceeded()).isFalse();
     }
