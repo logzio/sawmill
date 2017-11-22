8559bdc6c9584768f86fe3650dec2ff3af279567
guy93r <guy.r@logz.io>
2017-11-22 12:18:13 +0200

    tailor made for logz.io needs : only utf 8 encoding is supported and the code is now using guava super-fast utf 8 encoding

--- sawmill-core/src/main/java/io/logz/sawmill/processors/DocSizeProcessor.java
+++ sawmill-core/src/main/java/io/logz/sawmill/processors/DocSizeProcessor.java
@@ -1,24 +1,17 @@
 package io.logz.sawmill.processors;
 
+import com.google.common.base.Utf8;
 import io.logz.sawmill.Doc;
 import io.logz.sawmill.ProcessResult;
 import io.logz.sawmill.Processor;
 import io.logz.sawmill.annotations.ProcessorProvider;
 import io.logz.sawmill.utilities.JsonUtils;
-
-import java.nio.charset.Charset;
-import java.nio.charset.UnsupportedCharsetException;
 import java.util.Map;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 @ProcessorProvider(type = "size", factory = DocSizeProcessor.Factory.class)
 public class DocSizeProcessor implements Processor {
 
-    private final String charset;
-    public DocSizeProcessor(String charSet){
-        this.charset = checkNotNull(charSet, "Character set cannot be null");
-    }
+    public DocSizeProcessor(){}
 
     @Override
     public ProcessResult process(Doc doc) {
@@ -26,13 +19,7 @@ public class DocSizeProcessor implements Processor {
             return ProcessResult.failure("docSize field already exists");
         }
         String sourceAsJsonString = JsonUtils.toJsonString(doc.getSource());
-        Charset charSetForName;
-        try{
-            charSetForName = Charset.forName(this.charset);
-        } catch (UnsupportedCharsetException e){
-            return ProcessResult.failure(String.format("Unsupported characterSet %s", charset));
-        }
-        doc.addField("docSize", charSetForName.encode(sourceAsJsonString).limit());
+        doc.addField("docSize", Utf8.encodedLength(sourceAsJsonString));
         return ProcessResult.success();
     }
 
@@ -41,22 +28,7 @@ public class DocSizeProcessor implements Processor {
         public Factory(){}
 
         @Override
-        public DocSizeProcessor create(Map<String, Object> config) {
-            DocSizeProcessor.Configuration processorConfig = JsonUtils.fromJsonMap(DocSizeProcessor.Configuration.class, config);
-            return new DocSizeProcessor(processorConfig.getCharset());
-        }
-    }
-
-    public static class Configuration implements Processor.Configuration {
-        private String charset;
-        public Configuration() { }
-
-        public Configuration(String charset){
-            this.charset = charset;
-        }
-
-        public String getCharset() {
-            return charset;
+        public DocSizeProcessor create(Map<String, Object> config) { return new DocSizeProcessor();
         }
     }
 }
--- sawmill-core/src/test/java/io/logz/sawmill/processors/DocSizeProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/DocSizeProcessorTest.java
@@ -1,12 +1,10 @@
 package io.logz.sawmill.processors;
 
+import com.google.common.base.Utf8;
 import io.logz.sawmill.Doc;
 import io.logz.sawmill.ProcessResult;
 import io.logz.sawmill.utilities.JsonUtils;
 import org.junit.Test;
-
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 import static io.logz.sawmill.utils.DocUtils.createDoc;
 import static io.logz.sawmill.utils.FactoryUtils.createConfig;
@@ -29,68 +27,26 @@ public class DocSizeProcessorTest {
         String s = "Hello, UTF-8!";
         Doc doc = createDoc("testField", s);
 
-        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class, createConfig("charset", "UTF-8"));
+        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class);
         ProcessResult processResult = sizeProcessor.process(doc);
 
         assertThat(processResult.isSucceeded()).isTrue();
         assertThat(doc.hasField("docSize")).isTrue();
-        assertThat((int) doc.getField("docSize")).isEqualTo(
-                StandardCharsets.UTF_8.encode(JsonUtils.toJsonString(createDoc("testField", s).getSource())).limit()
-        );
+        assertThat((int) doc.getField("docSize")).isEqualTo(Utf8.encodedLength(JsonUtils.toJsonString(createDoc("testField", s).getSource())));
     }
 
-    @Test
-    public void utf16CharacterSet(){
-        String s = "Hello, UTF-16!";
-        Doc doc = createDoc("testField", s);
-
-        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class, createConfig("charset", "UTF-16"));
-        ProcessResult processResult = sizeProcessor.process(doc);
-
-        assertThat(processResult.isSucceeded()).isTrue();
-        assertThat(doc.hasField("docSize")).isTrue();
-        assertThat((int) doc.getField("docSize")).isEqualTo(
-                StandardCharsets.UTF_16.encode(JsonUtils.toJsonString(createDoc("testField", s).getSource())).limit()
-        );
-    }
-
-    @Test
-    public void asciiCharacterSet(){
-        String s = "Hello, ASCII!";
-        Doc doc = createDoc("testField", s);
-
-        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class, createConfig("charset", "ASCII"));
-        ProcessResult processResult = sizeProcessor.process(doc);
-
-        assertThat(processResult.isSucceeded()).isTrue();
-        assertThat(doc.hasField("docSize")).isTrue();
-        assertThat((int) doc.getField("docSize")).isEqualTo(
-                StandardCharsets.US_ASCII.encode(JsonUtils.toJsonString(createDoc("testField", s).getSource())).limit()
-        );
-    }
 
     @Test
     public void differentLangTest(){
         String s = "こんにちは世界!";
         Doc doc = createDoc("testField", s);
 
-        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class, createConfig("charset", "UTF-8"));
+        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class);
         ProcessResult processResult = sizeProcessor.process(doc);
 
         assertThat(processResult.isSucceeded()).isTrue();
         assertThat(doc.hasField("docSize")).isTrue();
-        assertThat((int) doc.getField("docSize")).isEqualTo(
-                StandardCharsets.UTF_8.encode(JsonUtils.toJsonString(createDoc("testField", s).getSource())).limit()
-        );
+        assertThat((int) doc.getField("docSize")).isEqualTo(Utf8.encodedLength(JsonUtils.toJsonString(createDoc("testField", s).getSource())));
     }
 
-    @Test
-    public void nonStandartCharSet(){
-        String s = "こんにちは世界!";
-        Doc doc = createDoc("testField", s);
-
-        DocSizeProcessor sizeProcessor = createProcessor(DocSizeProcessor.class, createConfig("charset", "asdfasdf"));
-        ProcessResult processResult = sizeProcessor.process(doc);
-        assertThat(processResult.isSucceeded()).isFalse();
-    }
 }
