2c57386be33ffbc6ea9b3b5260fc8436f3ad378d
oririechman <ori@logz.io>
2018-05-14 18:06:58 +0300

    PR comments

--- sawmill-core/src/main/java/io/logz/sawmill/Template.java
+++ sawmill-core/src/main/java/io/logz/sawmill/Template.java
@@ -20,7 +20,7 @@ public class Template {
         if (doc == null) {
             docContext = Collections.EMPTY_MAP;
         } else {
-            docContext = doc.getFlattenSource();
+            docContext = doc.getSource();
         }
 
         StringWriter writer = new StringWriter();
