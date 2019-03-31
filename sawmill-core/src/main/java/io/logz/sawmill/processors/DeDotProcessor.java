9ae6eea9fe1d6bb3ee23a368741229cd0c74a2c8
avishayhirsh <avishay.hirsh@logz.io>
2019-03-31 16:42:07 +0300

    Create Dedot Processor.

--- sawmill-core/src/main/java/io/logz/sawmill/processors/DeDotProcessor.java
+++ sawmill-core/src/main/java/io/logz/sawmill/processors/DeDotProcessor.java
@@ -3,43 +3,112 @@ package io.logz.sawmill.processors;
 import io.logz.sawmill.Doc;
 import io.logz.sawmill.ProcessResult;
 import io.logz.sawmill.Processor;
+import io.logz.sawmill.annotations.ProcessorProvider;
+import io.logz.sawmill.exceptions.ProcessorConfigurationException;
+import io.logz.sawmill.utilities.JsonUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+@ProcessorProvider(type = "de_dot", factory = DeDotProcessor.Factory.class)
 public class DeDotProcessor implements Processor {
-    private String fieldSplit="_";
+    private String fieldSplit;
+    private static final Logger logger = LoggerFactory.getLogger(DeDotProcessor.class);
 
     public DeDotProcessor(String fieldSplit) {
-        this.fieldSplit = !StringUtils.isEmpty(fieldSplit) ? fieldSplit:this.fieldSplit ;
+        this.fieldSplit = fieldSplit;
     }
 
     @Override
-    public ProcessResult process(Doc doc) throws InterruptedException {
+    public ProcessResult process(Doc doc) {
         Map<String,Object> docAsMap = doc.getSource();
         deDotMap(docAsMap);
         return ProcessResult.success();
     }
 
     private void deDotMap(Map<String, Object> docAsMap) {
+        //Creating a copy of the Map in this level , and also in each level in the recursive level is required
+        //in order to avoid ConcurrentModificationException , because in our case we want to replace the key thus actually we remove & add .
+        //add is not supported while iterating , and the end result is to create a copy of the map.
+        Map<String,Object> mapClone = new LinkedHashMap<>();
+        deDotInner(docAsMap,mapClone);
+        docAsMap.clear();
+        docAsMap.putAll(mapClone);
+    }
+
+    private void deDotInner(Map<String, Object> docAsMap, Map<String,Object> mapClone) {
         Iterator<Map.Entry<String,Object>> iterator = docAsMap.entrySet().iterator();
+
         while(iterator.hasNext()){
             Map.Entry<String,Object> entry = iterator.next();
+            String dedotedKey = dedot(entry.getKey());
             if(entry.getValue() instanceof Map){
-                deDotMap((Map)entry.getValue());
+                Map<String,Object> newInnerMap = new LinkedHashMap<>();
+                mapClone.put(dedotedKey,newInnerMap);
+                deDotInner((Map)entry.getValue(),newInnerMap);
             }
             else if(entry.getValue() instanceof List && !((List) entry.getValue()).isEmpty() && ((List) entry.getValue()).get(0) instanceof Map){
-                ((List) entry.getValue()).stream().forEach(map -> deDotMap((Map<String, Object>) map));
+                List<Map<String,Object>> newInnerMapList = new ArrayList<>();
+                mapClone.put(dedotedKey,newInnerMapList);
+                ((List) entry.getValue()).stream().forEach(map ->
+                {
+                    Map<String,Object> newInnerMap = new LinkedHashMap<>();
+                    newInnerMapList.add(newInnerMap);
+                    deDotInner((Map<String, Object>) map,newInnerMap);
+                });
+            }else{
+                mapClone.put(dedotedKey,entry.getValue());
             }
-            String originalKey = entry.getKey();
-            if(originalKey.contains(".")){
-                Object valueBackup = entry.getValue();
-                iterator.remove();
-                String newKey = originalKey.replace(".",fieldSplit);
-                docAsMap.put(newKey,valueBackup);
+        }
+    }
+
+    private String dedot(String originalKey) {
+        String dedotedKey = originalKey;
+        if(dedotedKey.contains(".")){
+            dedotedKey = dedotedKey.replace(".",fieldSplit);
+        }
+        return dedotedKey;
+    }
+
+    public static class Factory implements Processor.Factory {
+
+        @Override
+        public Processor create(Map<String,Object> config) {
+            if(config!=null){
+                DeDotProcessor.Configuration dedotConfiguration = null;
+                try{
+                    dedotConfiguration = JsonUtils.fromJsonMap(Configuration.class, config);
+                }
+                catch (Exception ex){
+                    logger.error("failed to parse dedot processor confi",ex);
+                    throw new ProcessorConfigurationException("failed to parse dedot processor config");
+                }
+
+                if(StringUtils.isNotEmpty(dedotConfiguration.getSeperator())){
+                    return new DeDotProcessor(dedotConfiguration.getSeperator());
+                }
             }
+            return new DeDotProcessor(Configuration.DEDOT_DEFAULT_VAL);
+        }
+    }
+
+    public static class Configuration implements Processor.Configuration {
+        public final static String DEDOT_DEFAULT_VAL = "_";
+        private String seperator;
+
+        public Configuration() { }
+
+        public Configuration(String seperator) {
+            this.seperator = seperator;
+        }
+        public String getSeperator() {
+            return seperator;
         }
     }
 }
--- sawmill-core/src/test/java/io/logz/sawmill/processors/DeDotProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/DeDotProcessorTest.java
@@ -1,4 +1,128 @@
 package io.logz.sawmill.processors;
 
+import io.logz.sawmill.Doc;
+import io.logz.sawmill.utilities.JsonUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static io.logz.sawmill.utils.FactoryUtils.createConfig;
+import static io.logz.sawmill.utils.FactoryUtils.createProcessor;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DeDotProcessorTest {
+
+    String messageExample = "{\n" +
+            "  \"inner.object\": {\n" +
+            "    \"_id\": \"5c9ce370f7edc93603cfcc5a\",\n" +
+            "    \"index\": 3,\n" +
+            "    \"guid\": \"6028894d-1aca-4c25-9b65-bb9d496d3b93\",\n" +
+            "    \"isActive\": false,\n" +
+            "    \"balance\": \"$2,333.93\",\n" +
+            "    \"picture\": \"http://placehold.it/32x32\",\n" +
+            "    \"age\": 36,\n" +
+            "    \"eyeColor\": \"blue\",\n" +
+            "    \"name\": {\n" +
+            "      \"first\": \"Robin\",\n" +
+            "      \"last\": \"Mckay\"\n" +
+            "    },\n" +
+            "    \"company\": \"FITCORE\",\n" +
+            "    \"email\": \"robin.mckay@fitcore.ca\",\n" +
+            "    \"phone\": \"+1 (998) 522-3511\",\n" +
+            "    \"address\": \"536 Bennet Court, Mappsville, District Of Columbia, 5733\",\n" +
+            "    \"about.us\": \"Ut id sint sint proident laboris exercitation cupidatat deserunt aliquip eu cillum officia. Culpa sit ex irure officia eu qui. Non excepteur aliqua voluptate sint magna esse elit consequat mollit mollit reprehenderit dolor. Nisi sit laboris ullamco culpa ullamco laborum commodo fugiat commodo eu aliqua Lorem eiusmod amet. Voluptate sunt consequat mollit quis anim fugiat cillum mollit dolor pariatur. Ipsum ut esse elit tempor. Do ea amet eiusmod laboris tempor deserunt.\",\n" +
+            "    \"registered\": \"Tuesday, March 8, 2016 2:35 PM\",\n" +
+            "    \"gps.latitude\": \"-83.019706\",\n" +
+            "    \"longitude\": \"6.023174\",\n" +
+            "    \"tags\": [\n" +
+            "      \"mollit\",\n" +
+            "      \"qui\",\n" +
+            "      \"anim\",\n" +
+            "      \"pariatur\",\n" +
+            "      \"pariatur\"\n" +
+            "    ],\n" +
+            "    \"range\": [\n" +
+            "      0,\n" +
+            "      1,\n" +
+            "      2,\n" +
+            "      3,\n" +
+            "      4,\n" +
+            "      5,\n" +
+            "      6,\n" +
+            "      7,\n" +
+            "      8,\n" +
+            "      9\n" +
+            "    ],\n" +
+            "    \"friends\": [{\n" +
+            "        \"id\": 0,\n" +
+            "        \"first.name\": \"Sheena James\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 1,\n" +
+            "        \"first.name\": \"Castaneda Hinton\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 2,\n" +
+            "        \"first.name\": \"Della Curry\"\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"greeting\": \"Hello, Robin! You have 10 unread messages.\",\n" +
+            "    \"favoriteFruit\": \"banana\"\n" +
+            "  },\n" +
+            "  \"_id\": \"5c9ce370543222f1cf6b53e0\",\n" +
+            "  \"index\": 0,\n" +
+            "  \"guid\": \"d055c342-4c62-4af3-944d-541a835f79a4\",\n" +
+            "  \"isActive\": false,\n" +
+            "  \"balance\": \"$3,331.30\",\n" +
+            "  \"picture\": \"http://placehold.it/32x32\",\n" +
+            "  \"age\": 29,\n" +
+            "  \"eyeColor\": \"blue\",\n" +
+            "  \"first.name\": {\n" +
+            "    \"first\": \"Magdalena\",\n" +
+            "    \"last\": \"Stewart\"\n" +
+            "  }\n" +
+            "}";
+
+    @Test
+    public void testDefaultSeperator() {
+
+        //Map<String, Object> config = createConfig("seperator","-");
+        AssertOnSeperator(messageExample, null);
+    }
+    @Test
+    public void testCustomSeperator() {
+
+        Map<String, Object> config = createConfig("seperator","_");
+        AssertOnSeperator(messageExample, config);
+
+        config = createConfig("seperator","#");
+        AssertOnSeperator(messageExample, config);
+    }
+
+
+    private void AssertOnSeperator(String messageExample, Map<String, Object> config) {
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class,messageExample));
+
+        Object obk = doc.getSource().get("inner.object");
+
+        assertThat(doc.getSource().get("inner.object")).isNotNull();
+        assertThat(doc.getSource().get("first.name")).isNotNull();
+        assertThat(((Map<String,Object>) doc.getSource().get("inner.object")).get("gps.latitude")).isNotNull();
+        assertThat(((Map<String,Object>) doc.getSource().get("inner.object")).get("about.us")).isNotNull();
+
+        DeDotProcessor deDotProcessor = createProcessor(DeDotProcessor.class, config);
+
+        assertThat(deDotProcessor.process(doc).isSucceeded()).isTrue();
+
+        assertThat(doc.getSource().get("inner.object")).isNull();
+        assertThat(doc.getSource().get("first.name")).isNull();
+
+        String seperator = config!=null ? (String)config.get("seperator") : DeDotProcessor.Configuration.DEDOT_DEFAULT_VAL;
+
+        assertThat(doc.getSource().get("first"+ seperator +"name")).isNotNull();
+        assertThat(((Map<String,Object>) doc.getSource().get("inner"+ seperator +"object")).get("gps"+ seperator +"latitude")).isNotNull();
+        assertThat(((Map<String,Object>) doc.getSource().get("inner"+ seperator +"object")).get("about"+ seperator +"us")).isNotNull();
+        assertThat(doc.getSource().get("inner"+ seperator +"object")).isNotNull();
+    }
 }
