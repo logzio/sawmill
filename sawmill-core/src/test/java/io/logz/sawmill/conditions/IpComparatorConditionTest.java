bed29a1312919e4b980bd62a8f4f0d46c5ff7fff
jgreen117 <Justin.g@logz.io>
2022-04-04 10:26:03 -0400

    Create IpComparatorConditionTest.java

---
+++ sawmill-core/src/test/java/io/logz/sawmill/conditions/IpComparatorConditionTest.java
@@ -0,0 +1,45 @@
+package io.logz.sawmill.conditions;
+
+import io.logz.sawmill.Doc;
+import io.logz.sawmill.parser.ConditionParser;
+import io.logz.sawmill.utils.FactoryUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static io.logz.sawmill.utils.DocUtils.createDoc;
+import static io.logz.sawmill.utils.FactoryUtils.createConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IpComparatorConditionTest {
+	
+	public ConditionParser conditionParser;
+
+    @Before
+    public void init() {
+        conditionParser = new ConditionParser(FactoryUtils.defaultConditionFactoryRegistry);
+    }
+    
+    @Test
+    public void testIps() {
+        String field = "field1";
+        String ipHigh = "192.200.3.0";
+        String ipLow = "192.100.3.0";
+
+
+        Map<String, Object> config = createConfig("field", field,
+                "ipHigh", ipHigh, "ipLow", ipLow );
+        IpCompare ipComparatorCondition = new IpCompare.Factory().create(config, conditionParser);
+
+        Doc doc = createDoc("field1", "192.150.3.0");
+        assertThat(ipComparatorCondition.evaluate(doc)).isTrue();
+
+        doc = createDoc("field1", "192.300.3.0");
+        assertThat(ipComparatorCondition.evaluate(doc)).isFalse();
+
+        doc = createDoc("field1", "192.50.3.0");
+        assertThat(ipComparatorCondition.evaluate(doc)).isFalse();
+    }
+
+}
