c2f877300f28da94757cec985a3d04e5a3adc516
ron-logzio <ron.shay@logz.io>
2022-07-19 20:09:04 +0300

    DEV-32951 - update dataset + added test

--- sawmill-core/src/test/java/io/logz/sawmill/processors/FieldsNamesSignatureProcessorTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/FieldsNamesSignatureProcessorTest.java
@@ -5,6 +5,8 @@ import io.logz.sawmill.ProcessResult;
 import io.logz.sawmill.utilities.JsonUtils;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,8 +26,7 @@ public class FieldsNamesSignatureProcessorTest {
             "\t\"loglevel\": \"INFO\",\n" +
             "\t\"pod-name\": \"log-analytics-56fc86d6f7-bpg4g\",\n" +
             "\t\"thread\": \"default-task-pool-50\",\n" +
-            "\t\"message\": \"started async task TaskExecution(executionId=AddNewAccountLogTypesTask-1971802461--791174232-88265380-8, taskKey=TaskKey(name=AddNewAccountLogTypesTask, group=es-logs-us-east-1d-prod-27, subGroup=default), params={clusterId=es-logs-us-east-1d-prod-27, addAccountLogTypesFromSecondsAgo=3900}, retryCount=0) on TaskKey(name=AddNewAccountLogTypesTask, group=es-logs-us-east-1d-prod-27, subGroup=default)\",\n" +
-            "\t\"type\": \"log-analytics\",\n" +
+            "\t\"message\": \"something happened\",\n" +
             "\t\"region\": \"us-east-1\",\n" +
             "\t\"listOfMaps\": [{\n" +
             "\t\t\"key1\": \"value1\",\n" +
@@ -42,91 +43,46 @@ public class FieldsNamesSignatureProcessorTest {
             "\t}],\n" +
             "\t\"nestedObject1\": {\n" +
             "\t\t\"field1\": 123,\n" +
-            "\t\t\"displayName\": \"Promise example\",\n" +
-            "\t\t\"name\": \"fs\",\n" +
-            "\t\t\"introduced_in\": \"v0.10.0\",\n" +
-            "\t\t\"stability\": 2,\n" +
-            "\t\t\"stabilityText\": \"Stable\",\n" +
             "\t\t\"nestedObject2\": {\n" +
             "\t\t\t\"field2\": \"123\",\n" +
             "\t\t\t\"name\": \"fs\",\n" +
             "\t\t\t\"displayName\": \"Promise example\",\n" +
-            "\t\t\t\"introduced_in\": \"v0.10.0\",\n" +
-            "\t\t\t\"stability\": 2,\n" +
-            "\t\t\t\"stabilityText\": \"Stable\",\n" +
             "\t\t\t\"nestedObject3\": {\n" +
             "\t\t\t\t\"field3\": \"poasdsa\",\n" +
             "\t\t\t\t\"name\": \"fs\",\n" +
             "\t\t\t\t\"displayName\": \"Promise example\",\n" +
-            "\t\t\t\t\"introduced_in\": \"v0.10.0\",\n" +
-            "\t\t\t\t\"stability\": 2,\n" +
-            "\t\t\t\t\"stabilityText\": \"Stable\",\n" +
-            "\t\t\t\t\"nestedObject4\": {\n" +
-            "\t\t\t\t\t\"field4\": \"12\",\n" +
-            "\t\t\t\t\t\"name\": \"fs\",\n" +
-            "\t\t\t\t\t\"displayName\": \"Promise example\",\n" +
-            "\t\t\t\t\t\"introduced_in\": \"v0.10.0\",\n" +
-            "\t\t\t\t\t\"stability\": 2,\n" +
-            "\t\t\t\t\t\"stabilityText\": \"Stable\",\n" +
-            "\t\t\t\t\t\"nestedObject5\": {\n" +
-            "\t\t\t\t\t\t\"field5\": \"12\",\n" +
-            "\t\t\t\t\t\t\"name\": \"fs\",\n" +
-            "\t\t\t\t\t\t\"displayName\": \"Promise example\",\n" +
-            "\t\t\t\t\t\t\"introduced_in\": \"v0.10.0\",\n" +
-            "\t\t\t\t\t\t\"stability\": 2,\n" +
-            "\t\t\t\t\t\t\"stabilityText\": \"Stable\"\n" +
-            "\t\t\t\t\t}\n" +
-            "\t\t\t\t}\n" +
+            "\t\t\t\t\"introduced_in\": \"v0.10.0\"\n" +
             "\t\t\t}\n" +
             "\t\t}\n" +
             "\t}\n" +
             "}";
 
-
     private final Set<String> fieldsNames = Stream.of(
-            "nestedObject1.nestedObject2.nestedObject3.nestedObject4.field4",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.stability",
+            "nestedObject1.field1",
                     "line",
                     "listOfMaps.key1",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.nestedObject5.introduced_in",
                     "logger",
                     "listOfMaps.innerMap2.innerKey2",
-                    "nestedObject1.nestedObject2.stability",
-                    "nestedObject1.nestedObject2.stabilityText",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.nestedObject5.name",
-                    "type",
-                    "nestedObject1.nestedObject2.nestedObject3.stabilityText",
+                    "thread",
+                    "message",
                     "nestedObject1.nestedObject2.field2",
                     "hostname",
                     "nestedObject1.nestedObject2.displayName",
-                    "LogSize",
-                    "listOfMaps.key",
-                    "nestedObject1.nestedObject2.nestedObject3.introduced_in",
-                    "nestedObject1.stability","listOfMaps.innerMap1.innerKey1",
-                    "nestedObject1.nestedObject2.nestedObject3.displayName",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.nestedObject5.displayName",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.name",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.introduced_in",
-                    "nestedObject1.name",
-                    "nestedObject1.field1",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.displayName",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.nestedObject5.stability",
-                    "thread",
-                    "message",
-                    "nestedObject1.stabilityText",
-                    "nestedObject1.introduced_in",
                     "@timestamp",
-                    "nestedObject1.nestedObject2.introduced_in",
+                    "LogSize",
                     "nestedObject1.nestedObject2.nestedObject3.name",
-                    "nestedObject1.nestedObject2.nestedObject3.stability",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.nestedObject5.field5",
-                    "loglevel","nestedObject1.displayName","nestedObject1.nestedObject2.nestedObject3.field3",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.stabilityText",
+                    "listOfMaps.key",
+                    "loglevel",
+                    "nestedObject1.nestedObject2.nestedObject3.introduced_in",
+                    "nestedObject1.nestedObject2.nestedObject3.field3",
                     "pod-name",
                     "region",
-                    "nestedObject1.nestedObject2.nestedObject3.nestedObject4.nestedObject5.stabilityText",
+                    "listOfMaps.innerMap1.innerKey1",
+                    "nestedObject1.nestedObject2.nestedObject3.displayName",
                     "nestedObject1.nestedObject2.name")
             .collect(Collectors.toSet());
+
+    private final String FIELDS_NAMES_SIGNATURE = "logzio_fields_signature";
 
     @Test
     public void testFieldsNamesSignature() throws InterruptedException {
@@ -135,8 +91,8 @@ public class FieldsNamesSignatureProcessorTest {
         Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
         ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
         assertThat(result.isSucceeded()).isTrue();
-        assertThat(doc.hasField("logzio_fields_signature")).isTrue();
-        int signature = doc.getField("logzio_fields_signature");
+        assertThat(doc.hasField(FIELDS_NAMES_SIGNATURE)).isTrue();
+        int signature = doc.getField(FIELDS_NAMES_SIGNATURE);
         String type = doc.hasField("type") ? doc.getField("type") : "";
         assertThat(signature).isEqualTo((type + JsonUtils.toJsonString(fieldsNames)).hashCode());
     }
@@ -149,8 +105,8 @@ public class FieldsNamesSignatureProcessorTest {
         doc.removeField("type");
         ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
         assertThat(result.isSucceeded()).isTrue();
-        assertThat(doc.hasField("logzio_fields_signature")).isTrue();
-        int signature = doc.getField("logzio_fields_signature");
+        assertThat(doc.hasField(FIELDS_NAMES_SIGNATURE)).isTrue();
+        int signature = doc.getField(FIELDS_NAMES_SIGNATURE);
         fieldsNames.remove("type");
         assertThat(signature).isEqualTo((JsonUtils.toJsonString(fieldsNames)).hashCode());
     }
@@ -162,8 +118,23 @@ public class FieldsNamesSignatureProcessorTest {
         Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
         ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
         assertThat(result.isSucceeded()).isTrue();
-        assertThat(doc.hasField("logzio_fields_signature")).isTrue();
-        int signature = doc.getField("logzio_fields_signature");
+        assertThat(doc.hasField(FIELDS_NAMES_SIGNATURE)).isTrue();
+        int signature = doc.getField(FIELDS_NAMES_SIGNATURE);
         assertThat(signature).isEqualTo(JsonUtils.toJsonString(fieldsNames).hashCode());
+    }
+
+    @Test
+    public void testEmptyDoc() throws InterruptedException {
+        FieldsNamesSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(FieldsNamesSignatureProcessor.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", "value");
+        Doc doc = new Doc(map);
+        doc.removeField("key");
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(doc.hasField(FIELDS_NAMES_SIGNATURE)).isTrue();
+        int signature = doc.getField(FIELDS_NAMES_SIGNATURE);
+        assertThat(signature).isEqualTo(JsonUtils.toJsonString(Collections.emptySet()).hashCode());
     }
 }
