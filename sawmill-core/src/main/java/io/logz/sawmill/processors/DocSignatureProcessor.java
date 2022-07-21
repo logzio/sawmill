eced9e4f073930475b33fd0087ade48bba6c33e6
ron-logzio <ron.shay@logz.io>
2022-07-21 21:01:48 +0300

    DEV-32951 - add SignatureMode, tests

---
+++ sawmill-core/src/main/java/io/logz/sawmill/processors/DocSignatureProcessor.java
@@ -0,0 +1,138 @@
+package io.logz.sawmill.processors;
+
+import io.logz.sawmill.Doc;
+import io.logz.sawmill.ProcessResult;
+import io.logz.sawmill.Processor;
+import io.logz.sawmill.annotations.ProcessorProvider;
+import io.logz.sawmill.exceptions.ProcessorExecutionException;
+import io.logz.sawmill.utilities.JsonUtils;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@ProcessorProvider(type = "docSignature", factory = DocSignatureProcessor.Factory.class)
+public class DocSignatureProcessor implements Processor {
+
+    private final SignatureMode signatureMode;
+    private final Set<String> includeValueFields;
+    private final String DOC_SIGNATURE_FIELD = "logzio_doc_signature";
+    public DocSignatureProcessor(SignatureMode signatureMode, Set<String> includeValueFields) {
+        this.signatureMode = signatureMode;
+        this.includeValueFields = includeValueFields;
+    }
+
+    @Override
+    public ProcessResult process(Doc doc) throws InterruptedException {
+        String signature;
+        try {
+            signature = createSignature(doc);
+        } catch (Exception e) {
+            return ProcessResult.failure(
+                    "failed to create signature, SignatureMode: " + signatureMode,
+                    new ProcessorExecutionException(DocSignatureProcessor.class.getSimpleName(), e));
+        }
+
+        if(signature.isEmpty())
+            return ProcessResult.failure("signature is empty, SignatureMode: " + signatureMode);
+
+        addSignatureField(doc, signature);
+        return ProcessResult.success();
+
+    }
+
+    private String createSignature(Doc doc) throws InterruptedException {
+        switch(signatureMode) {
+            case FIELDS_VALUES:
+                return getFieldsValuesString(doc);
+            case FIELDS_NAMES:
+                return getFieldsNamesString(doc);
+            case HYBRID:
+                return getFieldsValuesString(doc) + getFieldsNamesString(doc);
+            default: return "";
+        }
+    }
+
+    private void addSignatureField(Doc doc, String signature) {
+        doc.addField(DOC_SIGNATURE_FIELD, signature.hashCode());
+    }
+
+    private String getFieldsNamesString(Doc doc) throws InterruptedException {
+        Set<String> fieldsNames = extractFieldsNames(doc);
+        return fieldsNames.isEmpty() ? "" : JsonUtils.toJsonString(fieldsNames);
+    }
+
+    private String getFieldsValuesString(Doc doc) {
+        Set<String> values = includeValueFields.stream()
+                .filter(doc::hasField)
+                .map(doc::getField)
+                .map(Object::toString)
+                .collect(Collectors.toSet());
+        return values.isEmpty() ? "" : JsonUtils.toJsonString(values);
+    }
+
+    private Set<String> extractFieldsNames(Doc doc) throws InterruptedException {
+        Map<String, Object> source = doc.getSource();
+        Set<String> fields = new HashSet<>();
+        extractFieldsNames(source, null, fields);
+        return fields;
+    }
+
+    public void extractFieldsNames(Object object, String key, Set<String> fields) throws InterruptedException {
+        if (Thread.currentThread().isInterrupted()) throw new InterruptedException();
+        if(object instanceof Map) {
+            Map<String, Object> map = (Map) object;
+            for(Map.Entry<String, Object> entry : map.entrySet()) {
+                extractFieldsNames(entry.getValue(),
+                        key != null ?
+                                new StringBuilder(key).append('.').append(entry.getKey()).toString() : entry.getKey(),
+                        fields);
+            }
+        } else if(isListOfMaps(object)) {
+            List<Map<String, Object>> listOfMaps = (List<Map<String, Object>>) object;
+            for(Map<String, Object> map : listOfMaps) {
+                extractFieldsNames(map, key, fields);
+            }
+        } else {
+            fields.add(key);
+        }
+    }
+
+    private boolean isListOfMaps(Object object) {
+        return object instanceof List && !((List) object).isEmpty() && ((List) object).get(0) instanceof Map;
+    }
+
+    public static class Factory implements Processor.Factory {
+        public Factory() {}
+
+        @Override
+        public DocSignatureProcessor create(Map<String,Object> config) {
+            DocSignatureProcessor.Configuration fieldsNamesSignatureConfig =
+                    JsonUtils.fromJsonMap(DocSignatureProcessor.Configuration.class, config);
+            return new DocSignatureProcessor(fieldsNamesSignatureConfig.signatureMode,
+                    fieldsNamesSignatureConfig.getIncludeValueFields());
+        }
+    }
+
+    public enum SignatureMode {
+        FIELDS_NAMES,
+        FIELDS_VALUES,
+        HYBRID
+    }
+
+    public static class Configuration implements Processor.Configuration {
+
+        private SignatureMode signatureMode = SignatureMode.FIELDS_NAMES;
+        private Set<String> includeValueFields = new HashSet<>();
+        public Configuration(Set<String> includeValueFields, SignatureMode signatureMode) {
+            this.signatureMode = signatureMode;
+            this.includeValueFields = includeValueFields;
+        }
+        public Configuration() {}
+        public SignatureMode getSignatureMode() { return signatureMode; }
+        public Set<String> getIncludeValueFields() { return includeValueFields; }
+    }
+
+}
---
+++ sawmill-core/src/test/java/io/logz/sawmill/processors/DocSignatureProcessorTest.java
@@ -0,0 +1,241 @@
+package io.logz.sawmill.processors;
+
+import com.google.common.collect.Sets;
+import io.logz.sawmill.Doc;
+import io.logz.sawmill.ProcessResult;
+import io.logz.sawmill.utilities.JsonUtils;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static io.logz.sawmill.utils.FactoryUtils.createProcessor;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DocSignatureProcessorTest {
+
+    private final String message = "{\n" +
+            "\t\"hostname\": \"log-analytics-56fc86d6f7-bpg4g\",\n" +
+            "\t\"@timestamp\": \"2022-07-14T14:00:02.766Z\",\n" +
+            "\t\"LogSize\": 730,\n" +
+            "\t\"line\": \"133\",\n" +
+            "\t\"logger\": \"io.logz.taskz.core.TaskzExecutor\",\n" +
+            "\t\"loglevel\": \"INFO\",\n" +
+            "\t\"pod-name\": \"log-analytics-56fc86d6f7-bpg4g\",\n" +
+            "\t\"thread\": \"default-task-pool-50\",\n" +
+            "\t\"message\": \"something happened\",\n" +
+            "\t\"region\": \"us-east-1\",\n" +
+            "\t\"listOfMaps\": [{\n" +
+            "\t\t\"key1\": \"value1\",\n" +
+            "\t\t\"innerMap1\": {\n" +
+            "\t\t\t\"innerKey1\": \"innerValue1\"\n" +
+            "\t\t},\n" +
+            "\t\t\"key\": \"value\"\n" +
+            "\t}, {\n" +
+            "\t\t\"key1\": \"value1\",\n" +
+            "\t\t\"innerMap2\": {\n" +
+            "\t\t\t\"innerKey2\": \"innerValue2\"\n" +
+            "\t\t},\n" +
+            "\t\t\"key\": \"value\"\n" +
+            "\t}],\n" +
+            "\t\"nestedObject1\": {\n" +
+            "\t\t\"field1\": 123,\n" +
+            "\t\t\"nestedObject2\": {\n" +
+            "\t\t\t\"field2\": \"123\",\n" +
+            "\t\t\t\"name\": \"fs\",\n" +
+            "\t\t\t\"displayName\": \"Promise example\",\n" +
+            "\t\t\t\"nestedObject3\": {\n" +
+            "\t\t\t\t\"field3\": \"poasdsa\",\n" +
+            "\t\t\t\t\"name\": \"fs\",\n" +
+            "\t\t\t\t\"displayName\": \"Promise example\",\n" +
+            "\t\t\t\t\"introduced_in\": \"v0.10.0\"\n" +
+            "\t\t\t}\n" +
+            "\t\t}\n" +
+            "\t}\n" +
+            "}";
+
+    private final Set<String> fieldsNames = Stream.of(
+            "nestedObject1.field1",
+                    "line",
+                    "listOfMaps.key1",
+                    "logger",
+                    "listOfMaps.innerMap2.innerKey2",
+                    "thread",
+                    "message",
+                    "nestedObject1.nestedObject2.field2",
+                    "hostname",
+                    "nestedObject1.nestedObject2.displayName",
+                    "@timestamp",
+                    "LogSize",
+                    "nestedObject1.nestedObject2.nestedObject3.name",
+                    "listOfMaps.key",
+                    "loglevel",
+                    "nestedObject1.nestedObject2.nestedObject3.introduced_in",
+                    "nestedObject1.nestedObject2.nestedObject3.field3",
+                    "pod-name",
+                    "region",
+                    "listOfMaps.innerMap1.innerKey1",
+                    "nestedObject1.nestedObject2.nestedObject3.displayName",
+                    "nestedObject1.nestedObject2.name")
+            .collect(Collectors.toSet());
+
+    private final String DOC_SIGNATURE_FIELD = "logzio_doc_signature";
+    private final Set<String> includeValueFields = Stream.of("logger", "line").collect(Collectors.toSet());
+    private final Set<String> missingFields = Stream.of("noSuchField").collect(Collectors.toSet());
+
+    @Test
+    public void testFieldsNamesSignature() throws InterruptedException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("signatureMode", DocSignatureProcessor.SignatureMode.FIELDS_NAMES);
+
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
+
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isTrue();
+
+        int signature = doc.getField(DOC_SIGNATURE_FIELD);
+        String expectedSignature = JsonUtils.toJsonString(fieldsNames);
+        assertThat(signature).isEqualTo(expectedSignature.hashCode());
+    }
+
+    @Test
+    public void testFieldsValuesSignature() throws InterruptedException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("signatureMode", DocSignatureProcessor.SignatureMode.FIELDS_VALUES);
+        config.put("includeValueFields", includeValueFields);
+
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
+
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isTrue();
+
+        int signature = doc.getField(DOC_SIGNATURE_FIELD);
+        String expectedSignature = JsonUtils.toJsonString(getFieldsValues(doc, includeValueFields));
+        assertThat(signature).isEqualTo(expectedSignature.hashCode());
+    }
+
+    @Test
+    public void testHybridSignature() throws InterruptedException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("signatureMode", DocSignatureProcessor.SignatureMode.HYBRID);
+        config.put("includeValueFields", includeValueFields);
+
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
+
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isTrue();
+
+        int signature = doc.getField(DOC_SIGNATURE_FIELD);
+        Set<String> values = getFieldsValues(doc, includeValueFields);
+        String expectedSignature = JsonUtils.toJsonString(values) + JsonUtils.toJsonString(fieldsNames);
+        assertThat(signature).isEqualTo(expectedSignature.hashCode());
+    }
+
+    @Test
+    public void testFieldsNamesEmptySignature() throws InterruptedException {
+        //should test for each SignatureMode ?
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", "value");
+        Doc doc = new Doc(map);
+        doc.removeField("key");
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+        assertThat(result.isSucceeded()).isFalse();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isFalse();
+    }
+
+    @Test
+    public void testFieldsValuesEmptySignature() throws InterruptedException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("signatureMode", DocSignatureProcessor.SignatureMode.FIELDS_VALUES);
+        config.put("includeValueFields", missingFields);
+
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
+
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+
+        assertThat(result.isSucceeded()).isFalse();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isFalse();
+    }
+
+    @Test
+    public void testFieldsValuesSignatureMissingFields() throws InterruptedException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("signatureMode", DocSignatureProcessor.SignatureMode.FIELDS_VALUES);
+        config.put("includeValueFields", Sets.union(includeValueFields, missingFields));
+
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
+
+        ProcessResult result = fieldsNamesSignatureProcessor.process(doc);
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isTrue();
+
+        int signature = doc.getField(DOC_SIGNATURE_FIELD);
+        String expectedSignature = JsonUtils.toJsonString(
+                getFieldsValues(doc, Sets.union(includeValueFields, missingFields)));
+        assertThat(signature).isEqualTo(expectedSignature.hashCode());
+    }
+
+    @Test
+    public void testFieldsValuesSignatureAddedField() throws InterruptedException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("signatureMode", DocSignatureProcessor.SignatureMode.FIELDS_VALUES);
+        config.put("includeValueFields", includeValueFields);
+
+        DocSignatureProcessor fieldsNamesSignatureProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        Doc doc = new Doc(JsonUtils.fromJsonString(Map.class, message));
+
+        ProcessResult firstResult = fieldsNamesSignatureProcessor.process(doc);
+
+        assertThat(firstResult.isSucceeded()).isTrue();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isTrue();
+
+        int firstSignature = doc.getField(DOC_SIGNATURE_FIELD);
+
+        Set<String> newIncludeValueFields = new HashSet<>(includeValueFields);
+        newIncludeValueFields.add("loglevel");
+        config.put("includeValueFields", newIncludeValueFields);
+
+        DocSignatureProcessor postAdditionProcessor =
+                createProcessor(DocSignatureProcessor.class, config);
+        doc.removeField(DOC_SIGNATURE_FIELD);
+
+        ProcessResult secondResult = postAdditionProcessor.process(doc);
+        assertThat(secondResult.isSucceeded()).isTrue();
+        assertThat(doc.hasField(DOC_SIGNATURE_FIELD)).isTrue();
+
+        int secondSignature = doc.getField(DOC_SIGNATURE_FIELD);
+        assertThat(firstSignature).isNotEqualTo(secondSignature);
+    }
+
+    private Set<String> getFieldsValues(Doc doc, Set<String> includeValueFields) {
+        return includeValueFields.stream()
+                .filter(doc::hasField)
+                .map(doc::getField)
+                .map(Object::toString)
+                .collect(Collectors.toSet());
+    }
+}
